730f888c8991b7fbb3640fb136c0236f948cd92d
olbaplegal <olbaplegal@gmail.com>
2025-05-17 14:39:48 -0300

    adicionando métodos put e get adicionais

--- APIEstacionamento/APIEstacionamento/Controllers/EstacionamentoController.cs
+++ APIEstacionamento/APIEstacionamento/Controllers/EstacionamentoController.cs
@@ -3,6 +3,7 @@ using APIEstacionamento.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
 
 namespace APIEstacionamento.Controllers;
 
@@ -18,26 +19,40 @@ public class EstacionamentoController : ControllerBase
     }
 
     [HttpGet]
-    public ActionResult<IEnumerable<Carro>> Get()
+    public ActionResult<IEnumerable<Carro>> GetAll()
     {
         var carros = _context.Carros.ToList(); //trocar o Categorias depois de ajeitar o context
-        if(carros is null)
+        if (carros is null)
         {
             return NotFound("Carros não encontrados");
         }
         return carros;
     }
 
-    [HttpGet("{id:int}", Name="ObterProduto")]
-    public ActionResult<Carro> Get(int id)
+    [HttpGet("{id:int}", Name = "ObterProduto")]
+    public ActionResult<Carro> GetById(int id)
     {
         var carro = _context.Carros.FirstOrDefault(p => p.CarroId == id); // id do carro tem que ser igual ao id do request
-        if (carro == null) 
+        if (carro == null)
         {
             return NotFound("Carro não encontrado");
         }
         return carro;
     }
+
+    [HttpGet("BuscaSomenteCpf")]
+    public ActionResult<Carro> GetByCpf(string cpf)
+    {
+        var carro = _context.Carros.FirstOrDefault(c => c.Cpf == cpf);
+
+        if(carro is null)
+        {
+            return BadRequest("Cpf não cadastrado...");
+        }
+
+        return carro;
+    }
+    // precisa tratamento
 
     [HttpPost]
     public ActionResult Post(Carro carro)
@@ -49,14 +64,14 @@ public class EstacionamentoController : ControllerBase
 
         _context.Carros.Add(carro);
         _context.SaveChanges();
-        return new CreatedAtRouteResult("ObterProduto", 
-            new { id = carro.CarroId}, carro);
+        return new CreatedAtRouteResult("ObterProduto",
+            new { id = carro.CarroId }, carro);
     }
 
     [HttpPut("{id:int}")]
     public ActionResult Put(int id, Carro carro)
     {
-        if(id != carro.CarroId)
+        if (id != carro.CarroId)
         {
             return BadRequest();
         }
@@ -67,11 +82,86 @@ public class EstacionamentoController : ControllerBase
         return Ok(carro);
     }
 
+    [HttpPut("AtualizarComId")]
+    public ActionResult AtualizarCarroComId(Carro carro)
+    {
+        if(carro.CarroId == 0)
+        {
+            return BadRequest("Id do carro nulo...");
+        }
+
+        var carroVelho = _context.Carros.Where(w => w.CarroId == carro.CarroId);
+
+        if (carroVelho == null)
+        {
+            return BadRequest("não existe...");
+        }
+
+        _context.Entry(carro).State = EntityState.Modified;
+        _context.SaveChanges();
+
+
+        return Ok(carro);
+    }
+
+    [HttpPut("AtualizarSemId")]
+    public ActionResult AtualizarCarroSemId(Carro carro)
+    {
+        if (carro.Cpf is null || carro.Cpf.Trim().Equals(""))
+        {
+            return BadRequest("Cpf Motorista nulo...");
+        }
+
+        var carroVelho = _context.Carros
+            .Where(w => w.Cpf == carro.Cpf)
+            .AsNoTracking()
+            .FirstOrDefault();
+
+        if (carroVelho == null)
+        {
+            return BadRequest("não existe...");
+        }
+
+        carro.CarroId = carroVelho.CarroId;
+
+        //carroVelho.Cpf = carro.Cpf;
+        //carroVelho.Renavan = carro.Renavan;
+        //carroVelho.CarroId = carro.CarroId;
+        //carroVelho.Placa = carro.Placa;
+        //carroVelho.TipoCombustivel = carro.TipoCombustivel;
+
+        _context.Entry(carro).State = EntityState.Modified;
+        _context.SaveChanges();
+
+        return Ok(carro);
+    }
+
+    [HttpPut("AtualizarCombustivelComCpf")]
+    public ActionResult UpdateByCpf(string cpf, TipoCombustivel combustivel)
+    {
+        if (combustivel is 0)
+        {
+            return BadRequest("cpf nulo...");
+        }
+
+        var carro = _context.Carros.FirstOrDefault(c => c.Cpf == cpf);
+
+        if(carro is null)
+        {
+            return BadRequest("Cpf não cadastrado...");
+        }
+
+        carro.TipoCombustivel = combustivel;
+        _context.Entry(carro).State = EntityState.Modified;
+        _context.SaveChanges();
+        return Ok(carro);
+    }
+
     [HttpDelete("{id:int}")]
     public ActionResult Delete(int id)
     {
-        var carro = _context.Carros.FirstOrDefault(p=>p.CarroId == id);
-        if(carro is null)
+        var carro = _context.Carros.FirstOrDefault(p => p.CarroId == id);
+        if (carro is null)
         {
             return NotFound("carro não encontrado...");
         }
