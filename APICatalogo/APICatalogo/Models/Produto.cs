e29c36d4a5cf9004b6360d43539b0d28cba8a72b
pablo <olbaplegal@gmail.com>
2025-06-10 14:07:44 -0300

    aaaa

--- APICatalogo/APICatalogo/Models/Produto.cs
+++ APICatalogo/APICatalogo/Models/Produto.cs
@@ -30,7 +30,7 @@ public class Produto : IValidatableObject
     public Categoria? Categoria { get; set; }
 
     public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-    {
+    { 
         if (!string.IsNullOrEmpty(this.Nome))
         {
             var primeiraLetra = this.Nome[0].ToString();
