f906ce9f06b23f2f50651537bb7609f53e624e72
Pablo <olbaplegal@gmail.com>
2025-07-31 17:22:29 -0300

    migrando codigo sincrono para assincrono, mudando nomeclatura na interface Irepository e classe concreta repository

--- APICatalogo/APICatalogo/Repositories/Repository.cs
+++ APICatalogo/APICatalogo/Repositories/Repository.cs
@@ -12,31 +12,28 @@ public class Repository<T> : IRepository<T> where T : class
     {
         _context = context;
     }
-    public IEnumerable<T> GetAll()
+    public async Task<IEnumerable<T>> GetAllAsync()
     {
-       return _context.Set<T>().AsNoTracking().ToList();
+       return await _context.Set<T>().AsNoTracking().ToListAsync();
     }
-    public T? Get(Expression<Func<T, bool>> predicate)
+    public async Task<T?> GetAsync(Expression<Func<T, bool>> predicate)
     {
-        return _context.Set<T>().FirstOrDefault(predicate);
+        return await _context.Set<T>().FirstOrDefaultAsync(predicate);
     }
     public T Create(T entity)
     {
         _context.Set<T>().Add(entity);
-        //_context.SaveChanges();
         return entity;
     }
     public T Update(T entity)
     {
         _context.Set<T>().Update(entity);
-        //_context.SaveChanges();
-        return entity;
-    }
-    public T Delete(T entity)
-    {
-        _context.Set<T>().Remove(entity);
-        //_context.SaveChanges();
         return entity;
     }
 
+    public T Delete(T entity)
+    {
+        _context.Set<T>().Remove(entity);
+        return entity;
+    }
 }
