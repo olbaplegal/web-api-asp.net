0742634068eb63da911418a2884809ec335b5a63
Pablo <olbaplegal@gmail.com>
2025-07-31 17:45:23 -0300

    migrando interface IUnitOfWork e classe concreta

--- APICatalogo/APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bf1ccfa03f9231874237fdce366d9b14e24a222")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49e9fa5759c83f83457c8f8d249d116294e73d32")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
